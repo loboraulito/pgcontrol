c985e0ad0a0c18477554d8bbff7dfd0afc33ab7a
Raul Velasquez <raul.velasquez@uto.edu.bo>
2024-02-17 21:33:40 -0400

    Categoria (iniciando modulo de categoria)

--- src/main/java/bo/edu/uto/dtic/pgcontrol/mapas/CategoriaMapa.java
+++ src/main/java/bo/edu/uto/dtic/pgcontrol/mapas/CategoriaMapa.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Repository;
 import bo.edu.uto.dtic.pgcontrol.dominios.Categoria;
 
 @Repository
-public interface CategoriaMapper {
+public interface CategoriaMapa {
 	public List<Categoria> getAll();
-	public List<Categoria> getAllByIdEstado(Boolean id_estado);
 	public Categoria getById(Integer id);
 	public int insert(Categoria dato);
 	public int update(Categoria dato);
