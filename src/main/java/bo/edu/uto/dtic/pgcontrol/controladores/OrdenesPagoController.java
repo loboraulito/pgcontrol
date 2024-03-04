0e178567c13fa8e259a2dbab06e0840d433b973e
Raul Velasquez <raul.velasquez@uto.edu.bo>
2024-03-04 06:03:35 -0400

    Prototipo (añadiendo pantallas de prototipo)

---
+++ src/main/java/bo/edu/uto/dtic/pgcontrol/controladores/OrdenesPagoController.java
@@ -0,0 +1,40 @@
+package bo.edu.uto.dtic.pgcontrol.controladores;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import bo.edu.uto.dtic.pgcontrol.mapas.PersonasMapa;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author dtic
+ */
+@Controller
+@RequestMapping("/ordenes")
+public class OrdenesPagoController {
+
+	@Autowired
+	private PersonasMapa personasMapa;
+
+	@RequestMapping("/porPrograma")
+	@ResponseBody
+	public ModelAndView ficha_inscripcion(String term, HttpServletRequest hsr, HttpSession hs) {
+		HashMap modelo = new HashMap();
+		Integer id_usuario = (Integer) hs.getAttribute("__id_usuario");
+		return new ModelAndView("ordenes/porPrograma", modelo);
+	}
+	@RequestMapping("/listaPendientes")
+	@ResponseBody
+	public ModelAndView listaPendientes(String term, HttpServletRequest hsr, HttpSession hs) {
+		HashMap modelo = new HashMap();
+		Integer id_usuario = (Integer) hs.getAttribute("__id_usuario");
+		return new ModelAndView("ordenes/listaPendientes", modelo);
+	}
+}
